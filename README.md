23a564d80611205d295da2bd86ea7020145abb7a
Simon Wade <simon.wade@gmail.com>
2013-10-29 20:15:56 +1100

    Added example

---
+++ README.md
@@ -0,0 +1,9 @@
+# ember-spin.js
+
+An Ember.js component for displaying a spinning loading indicator using spin.js.
+
+BE AWARE that you'll need to define an empty `components/ic-spinner` template 
+or this won't work. See [this issue](https://github.com/emberjs/ember.js/issues/3231)
+if you (like me) think this is unfortunate.
+
+See [this JSBin for a demo](http://emberjs.jsbin.com/aYaLObe).
--- spinner.js
+++ spinner.js
@@ -1,14 +1,19 @@
 /**
  * An Ember.js component for displaying a spinning loading indicator using spin.js.
- * Implementation is Eric Berry's (@cavneb)
- * @see http://fgnass.github.io/spin.js/ for more info
+ * 
+ * Implementation is Eric Berry's (@cavneb), and I've just created the bower module.
+ * 
+ * BE AWARE that you'll need to define an empty `components/ic-spinner` template 
+ * or this won't work. See [this issue](https://github.com/emberjs/ember.js/issues/3231)
+ * if you (like me) think this is unfortunate.
+ * @see http://fgnass.github.io/spin.js/ for usage info
  */
 App.IcSpinnerComponent = Ember.Component.extend({
   
-  lines    : 13, // The number of lines to draw
-  length   : 9, // The length of each line
-  width    : 3, // The line thickness
-  radius   : 6, // The radius of the inner circle
+  lines    : 12, // The number of lines to draw
+  length   : 6, // The length of each line
+  width    : 2, // The line thickness
+  radius   : 4, // The radius of the inner circle
   corners  : 1, // Corner roundness (0..1)
   rotate   : 0, // The rotation offset
   direction: 1, // 1: clockwise, -1: counterclockwise
@@ -20,8 +25,9 @@ App.IcSpinnerComponent = Ember.Component.extend({
   className: 'spinner', // The CSS class to assign to the spinner
   zIndex   : 2e9, // The z-index (defaults to 2000000000)
   top      : 'auto', // Top position relative to parent in px
-  left     : 'auto' // Left position relative to parent in px
-  
+  left     : 'auto', // Left position relative to parent in px
+  template : Em.Handlebars.compile('{{foo}}'),
+
   showSpinner: function() {
     var target = this.get('element');
     this.spinner = new Spinner({
